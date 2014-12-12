67ad56df6841939ee8aaa091ccf5b0fac0bcc9d2
jonschlinkert <dev@sellside.com>
2014-12-12 05:34:48 -0500

    lint

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ var slice = require('array-slice');
 
 module.exports = function last(arr, num) {
   if (!Array.isArray(arr)) {
-    throw new Error('array-last expects an array as the first argument.')
+    throw new Error('array-last expects an array as the first argument.');
   }
 
   if (arr.length === 0) {
     return null;
   }
 
-  var res = slice(arr, arr.length - (isNumber(num) ? num : 1));
-  if (num === 1 || num == null) {
+  var res = slice(arr, arr.length - (isNumber(num) ? +num : 1));
+  if (+num === 1 || num == null) {
     return res[0];
   }
   return res;
