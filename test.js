f75ddb27026baedada00f7cbdac75aca9d858929
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-09 19:47:34 -0500

    run `update`

--- test.js
+++ test.js
@@ -11,12 +11,12 @@ require('mocha');
 var assert = require('assert');
 var last = require('./');
 
-describe('last', function () {
-  it('should throw an error if invalid args are passed:', function (cb) {
+describe('last', function() {
+  it('should throw an error if invalid args are passed:', function(cb) {
     try {
       last();
       cb(new Error('expected an error'));
-    } catch(err) {
+    } catch (err) {
       assert(err);
       assert(err.message);
       assert(err.message === 'expected the first argument to be an array');
@@ -24,12 +24,12 @@ describe('last', function () {
     }
   });
 
-  it('should return the last element in the array:', function () {
+  it('should return the last element in the array:', function() {
     assert.strictEqual(last(['a', 'b', 'c', 'd', 'e', 'f']), 'f');
     assert.strictEqual(last(['a', 'b', 'c', 'd', 'e', 'f'], 1), 'f');
   });
 
-  it('should the last n elements of the array:', function () {
+  it('should the last n elements of the array:', function() {
     assert.deepEqual(last(['a', 'b', 'c', 'd', 'e', 'f'], 3), ['d', 'e', 'f']);
   });
 });
